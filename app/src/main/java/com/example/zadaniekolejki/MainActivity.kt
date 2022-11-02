d0f3246b4d7dd754e26eb0fa7aa46c6fb3ff4085
matipoklo <poklojr@gmail.com>
2022-11-02 23:53:27 +0100

    Wykonany kod stosu

--- app/src/main/java/com/example/zadaniekolejki/MainActivity.kt
+++ app/src/main/java/com/example/zadaniekolejki/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.zadaniekolejki
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -47,7 +48,42 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             //Stos
-            
+            val stos = LinkedList<Int>()
+            val butt_dodaj_do_stosu = findViewById<Button>(R.id.btnDodaj3)
+            val butt_usun_ze_stosu = findViewById<Button>(R.id.btnUsun2)
+            val edittext_dodaj_do_stosu = findViewById<EditText>(R.id.editText_dodaj_do_stosu)
+            val txt_ile_w_stosie = findViewById<TextView>(R.id.textView7)
+            var ilosc_ele_w_stosie = 0
+
+            butt_dodaj_do_stosu.setOnClickListener {
+                if(edittext_dodaj_do_stosu.text.toString() != "") {
+                    val liczba = edittext_dodaj_do_stosu.text.toString().toInt()
+                    Toast.makeText(applicationContext, "Dodano do stosu liczbe: " + liczba, dlugosc).show()
+                    stos.add(liczba)
+                    ilosc_ele_w_stosie += 1
+                    txt_ile_w_stosie.text = ilosc_ele_w_stosie.toString()
+                }
+                else
+                {
+                    Toast.makeText(applicationContext, "Wpisz do pola nad przyciskiem, jaka liczbe chcesz dodac", dlugosc).show()
+                }
+
+            }
+
+            butt_usun_ze_stosu.setOnClickListener {
+                if (!stos.isEmpty())
+                {
+                    val liczba = stos.last()
+                    Toast.makeText(applicationContext, "usunieto do stosu liczbe: " + liczba, dlugosc).show()
+                    stos.removeLast()
+                    ilosc_ele_w_stosie -= 1
+                    txt_ile_w_stosie.text = ilosc_ele_w_stosie.toString()
+                }
+                else
+                {
+                    Toast.makeText(applicationContext, "Stos jest pusty", dlugosc).show()
+                }
+            }
         }
     }
 }
